1f834c9e5e4accd0284127d6fea79c341ff96c84
Yash Shrivastava <100514707+developerYash12@users.noreply.github.com>
2022-10-30 08:35:15 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var inpTxt = document.querySelector("#inp-txt");
+var outputTxt = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function translationUrl(input){
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(err) {
+    console.log("Error occured", err);
+    alert("Something Went Wrong! Try again after some time");
+}
+
+function clickHandler(){
+    var inputTxt = inpTxt.value;
+
+    fetch(translationUrl(inputTxt))
+    .then(response => response.json())
+    .then(json => { 
+        var translatedTxt = json.contents.translated;
+        outputTxt.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click",clickHandler);
